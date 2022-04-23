d72309a78fd943a2a20a996e52deabd0f1bcbe0b
Andreas Hakansson <andreas.hakansson@ohmy.se>
2022-04-23 18:33:40 +0200

    Fix issue when user does not belong to an academy

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,23 +6,34 @@
             </h2>
         </template>
 
-        <div class="py-10">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-wrap justify-between mb-2">
-                <div class="text-2xl">
-                    Latest training sessions at {{ academy.name }}
+        <template v-if="academy">
+            <div class="py-10">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-wrap justify-between mb-2">
+                    <div class="text-2xl">
+                        Latest training sessions at {{ academy.name }}
+                    </div>
+
+                    <div class="flex-shrink-0 flex">
+                        <Link :href="route('trainingSession.create')" as="button" type="button"
+                              class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                            New training session
+                        </Link>
+                    </div>
                 </div>
 
-                <div class="flex-shrink-0 flex">
-                    <Link :href="route('trainingSession.create')" as="button" type="button"
-                          class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
-                        New training session
-                    </Link>
+                <training-sessions :trainingSessions="trainingSessions"></training-sessions>
+
+            </div>
+        </template>
+        <template v-else>
+            <div class="py-10">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-wrap justify-between mb-2">
+                    <div class="text-2xl">
+                        You don't belong to any academy yet.
+                    </div>
                 </div>
             </div>
-
-            <training-sessions :trainingSessions="trainingSessions"></training-sessions>
-
-        </div>
+        </template>
     </app-layout>
 </template>
 
