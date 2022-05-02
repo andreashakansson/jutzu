f18f1f95ae449620cb75524fa8050783c1541a43
Andreas Hakansson <andreas.hakansson@ohmy.se>
2022-05-02 13:36:13 +0200

    Gray cancalled buttons try 5

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -1,7 +1,8 @@
 <template>
     <button :type="type"
-            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
-        :class="themeClasses"
+            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition"
+            :class="themeClasses"
+    >
         <slot></slot>
     </button>
 </template>
